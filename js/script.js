8558a9bcd4636e0a48bd462de4a35de879913020
Txaco <42583806+Txaco@users.noreply.github.com>
2018-09-28 17:59:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,48 @@
 const APP = (() => {
-  
-  const SHS = {};
-  SHS.defaults = { page: 0, pageSize: 100, format: 'json'};
-  SHS.endpoint = 'https://secondhandsongs.com/';
-	SHS.search_parameters = {
-			common: [`&page=${SHS.defaults.page}`, `&pageSize=${SHS.defaults.pageSize}`, `&format=${SHS.defaults.format}`],
-			work: [`?title=`, `&credits=`],
-			performance: [`?title=`, `&performer=`, `&date=`],
-			artist: [`?commonName=`],
-			object: [`?caption=`],
-			get_parameters(target, input, options) {
-				let parameters = this[target][0] + input;
+
+	// SecondHandSongs API Wrapper
+	const SHS = {};
+		SHS.endpoint = 'https://secondhandsongs.com/'; // Common endpoint URI
+		SHS.search_params = {}; // Search parameters - Common & specific for all entities, plus "get" method for output (as a string)
+			SHS.search_params.common = ['&page=0', '&pageSize=100', '&format=json']; // Common
+			SHS.search_params.work = ['?title=', '&credits=']; // Work entity
+			SHS.search_params.performance = ['?title=', '&performer=', '&date=']; // Performance entity
+			SHS.search_params.artist = ['?commonName=']; // Artist entity
+			SHS.search_params.object = ['?caption=']; // Object entity (generic, gives results for all entities)
+			// Method - Get request parameters as a string
+			SHS.search_params.get_string = function(target, input, options) {
+				let parameters = this[target][0] + input; // Start adding first target entity parameter (required) with value = input value
+				// Check options and add new parameters (if supported) with value = option value
 				if(options) {
-					if(target === 'work' && options.credits) parameters += this[target][1] + options.credits;
+					if(target === 'work' && options.credits) parameters += this[target][1] + options.credits; // Work supports "credits"
+					// Performance supports "performer" and "date"
 					else if(target === 'performance') {
 						if(options.performer) parameters += this[target][1] + options.performer;
 						if(options.date) parameters += this[target][2] + options.date;
 					}
 				}
-				parameters += this.common.join('');
-				return parameters;
-			}
+				parameters += this.common.join(''); // Finish adding common parameters
+				return parameters; // Output string
+			};
+		SHS.request_options = {}; // Fetch request options, including headers
+			SHS.request_options.method = 'GET'; // 'GET' | 'POST' | 'OPTIONS' | 'PUT' | 'DELETE'
+			SHS.request_options.mode = 'cors'; // 'cors' | 'no-cors' | 'same-origin'
+			SHS.request_options.credentials = 'omit'; // 'omit' | 'same-origin' | 'include'
+			SHS.request_options.referrer = 'client'; // 'no-referrer' | 'client' | '<URL>'
+			// SHS.request_options.referrerPolicy = 'origin';
+			// 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'unsafe-url'
+			SHS.request_options.headers = new Headers({ 'Accept': 'application/json' }); // Requets headers
+		// Method - Get search results by entity and input value, with supported options (parameters)
+		SHS.search = function(target, input, options) {
+			let parameters = this.search_params.get_string(target, input, options); // Get parameters
+			let uri = this.endpoint + 'search/' + target + parameters; // Build request URL
+			this.request(uri); // Fetch URL
 		};
-	SHS.search = function(target, input, options) {
-		let parameters = this.search_parameters.get_parameters(target, input, options);
-		let uri = this.endpoint + 'search/' + target + parameters;
-		this.request(uri);
-	};
-	SHS.request = function(uri) {
-		let options = {
-			headers: {
-				'Accept': 'application/json'
-			}
+		// Method - Fetch URL with request options, parse JSON response and LOG. Catch errors.
+		SHS.request = function(uri) {
+			fetch(uri, this.request_options).then(r => r.json()).then(d => console.log(d)).catch(e => console.log(e)); // Fetch URL
 		};
-		fetch(uri, options).then(r => r.json()).then(d => console.log(d)).catch(e => console.log(e));
-	};
 
-	SHS.search('object', 'blackbird');
+	SHS.search('object', 'blackbird'); // Search for all entities with this value in primary field
 
 })();
