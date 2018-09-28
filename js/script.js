21dfd911c3ede039aee10f19997724a26ee5309c
Txaco <42583806+Txaco@users.noreply.github.com>
2018-09-28 20:35:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ const APP = (() => {
 		SHS.request_options = {}; // Fetch request options, including headers
 			SHS.request_options.method = 'GET'; // 'GET' | 'POST' | 'OPTIONS' | 'PUT' | 'DELETE'
 			SHS.request_options.mode = 'same-origin'; // 'cors' | 'no-cors' | 'same-origin'
-			SHS.request_options.credentials = 'same-origin'; // 'omit' | 'same-origin' | 'include'
+			SHS.request_options.credentials = 'include'; // 'omit' | 'same-origin' | 'include'
 			//SHS.request_options.referrer = 'client'; // 'no-referrer' | 'client' | '<URL>'
 			//SHS.request_options.referrerPolicy = 'origin';
 			// 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'unsafe-url'
