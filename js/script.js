bac8fd046d5fb21394880b8c289e9fda09cf42d9
Txaco <42583806+Txaco@users.noreply.github.com>
2018-09-28 16:29:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+const APP = (() => {
+  
+  const SHS = {};
+  SHS.defaults = { page: 0, pageSize: 100, format: 'json'};
+  SHS.endpoint = 'https://secondhandsongs.com/';
+	SHS.search_parameters = {
+			common: [`&page=${SHS.defaults.page}`, `&pageSize=${SHS.defaults.pageSize}`, `&format=${SHS.defaults.format}`],
+			work: [`?title=`, `&credits=`],
+			performance: [`?title=`, `&performer=`, `&date=`],
+			artist: [`?commonName=`],
+			object: [`?caption=`],
+			get_parameters(target, input, options) {
+				let parameters = this[target][0] + input;
+				if(options) {
+					if(target === 'work' && options.credits) parameters += this[target][1] + options.credits;
+					else if(target === 'performance') {
+						if(options.performer) parameters += this[target][1] + options.performer;
+						if(options.date) parameters += this[target][2] + options.date;
+					}
+				}
+				parameters += this.common.join('');
+				return parameters;
+			}
+		};
+	SHS.search = function(target, input, options) {
+		let parameters = this.search_parameters.get_parameters(target, input, options);
+		let uri = this.endpoint + 'search/' + target + parameters;
+		this.request(uri);
+	};
+	SHS.request = function(uri) {
+		fetch(uri).then(r => r.json()).then(d => console.log(d)).catch(e => console.log(e));
+	};
 
+	SHS.search('object', 'blackbird');
+
+})();
