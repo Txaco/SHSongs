3e6cbb6efb7ece3b047ccedfab4d352e698c1981
Txaco <42583806+Txaco@users.noreply.github.com>
2018-09-30 01:42:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ const APP = (() => {
 			};
 			req.open('GET', uri, true);
 			req.setRequestHeader('Accept', 'application/json');
-			req.setRequestHeader('Origin', 'https://secondhandsongs.com/');
+			req.setRequestHeader('Content-Type', 'application/json');
 			//req.withCredentials = true;
 			console.log(req);
 			req.send('');
